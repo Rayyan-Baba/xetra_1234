1b38b7a30720df8481e154d96f5b8e235be6a148
Rayyan-Baba <rayyanwaheed6@gmail.com>
2025-03-28 13:35:25 +0300

    added read_csv_to_df and unittests

--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -36,8 +36,22 @@ class S3BucketConnector():
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
         return files       
     
-    def read_csv_to_df(self):
-        pass
+    def read_csv_to_df(self, key: str, encoding: str = 'utf-8', sep: str = ','):
+        """
+        reading a csv file from the s3 bucket and returning a dataframe
+
+        :param key: key of the file that should be read
+        :encoding: encoding of the data inside the csv file 
+        :sep: separator of the csv file 
+
+        returns:
+          data_frame: Pandas DataFrame containing the data of the csv file 
+        """
+        self._logger.info('Reading file %s/%s/%s', self.endpoint_url, self._bucket.name, key)
+        csv_obj = self._bucket.Object(key=key).get().get('Body').read().decode(encoding)
+        data = StringIO(csv_obj)
+        data_frame = pd.read_csv(data, sep=sep)
+        return data_frame
     
     def write_df_to_s3(self):
         pass
