0ed8e51e81dc5c20afbd57d810c3f7fba13d0b32
Rayyan-Baba <rayyanwaheed6@gmail.com>
2025-03-29 13:31:24 +0300

    added write_df_to_s3 and unittests

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -1,1 +1,9 @@
 """Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatExeption class
+
+    Exception that can be raised when the format type
+    given as parameter is not supported.
+    """
